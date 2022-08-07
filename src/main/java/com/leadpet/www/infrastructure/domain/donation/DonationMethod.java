885bcf9fbdeea67843771b76f77bdbcde8f0d116
shinemusCat <imhyuninny@gmail.com>
2022-08-07 18:08:54 +0900

    입양 카테고리 추가

--- src/main/java/com/leadpet/www/infrastructure/domain/donation/DonationMethod.java
+++ src/main/java/com/leadpet/www/infrastructure/domain/donation/DonationMethod.java
@@ -6,5 +6,8 @@ package com.leadpet.www.infrastructure.domain.donation;
 public enum DonationMethod {
     ACCOUNT, // 후원 계좌
     GOODS, // 물품 배송
+    FEED, // 사료
+    HYGIENE_PRODUCTS, // 위생용품
+    HOUSE, // 하우스
     ;
 }
